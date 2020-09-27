62505a0cf8276e62a80a35517f2799187546f239
Chris <spidergon@gmail.com>
2020-09-27 08:26:07 -0300

    created toppings content type and custom previews

--- sanity/schemas/schema.js
+++ sanity/schemas/schema.js
@@ -5,6 +5,7 @@ import createSchema from 'part:@sanity/base/schema-creator';
 import schemaTypes from 'all:part:@sanity/base/schema-type';
 
 import pizza from './pizza';
+import topping from './topping';
 
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
@@ -13,5 +14,5 @@ export default createSchema({
 
   // Then proceed to concatenate our document type
   // to the ones provided by any plugins that are installed
-  types: schemaTypes.concat([pizza]),
+  types: schemaTypes.concat([pizza, topping]),
 });
