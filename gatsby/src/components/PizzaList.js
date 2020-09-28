2b34d4ab99d7b25e3edd51a9f409059f5a9c2d20
Chris <spidergon@gmail.com>
2020-09-28 13:00:44 -0300

    Static Queries and built the toppings filter

--- gatsby/src/components/PizzaList.js
+++ gatsby/src/components/PizzaList.js
@@ -7,7 +7,7 @@ const PizzaGridStyles = styled.div`
   display: grid;
   grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
   gap: 4rem;
-  /* grid-auto-rows: auto auto 500px; */
+  grid-auto-rows: auto auto 500px;
 `;
 
 const PizzaStyles = styled.div`
---
+++ gatsby/src/components/ToppingsFilter.js
@@ -0,0 +1,84 @@
+import React from 'react';
+import { graphql, Link, useStaticQuery } from 'gatsby';
+import styled from 'styled-components';
+
+const ToppingStyles = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 1rem;
+  margin-bottom: 4rem;
+  a {
+    display: grid;
+    grid-template-columns: auto 1fr;
+    grid-gap: 0 1rem;
+    align-items: center;
+    padding: 5px;
+    background: var(--grey);
+    border-radius: 2px;
+    .count {
+      background: white;
+      padding: 2px 5px;
+    }
+    .active {
+      background: var(--yellow);
+    }
+  }
+`;
+
+function countPizzasInToppings(pizzas) {
+  // Return the pizzas with counts
+  const counts = pizzas
+    .map((pizza) => pizza.toppings)
+    .flat()
+    .reduce((acc, topping) => {
+      // Check if this is an existing topping
+      const existingTopping = acc[topping.id];
+      if (existingTopping) {
+        // if it is, increment by 1
+        existingTopping.count += 1;
+      } else {
+        // otherwise create a new entry in our acc and set it to one
+        acc[topping.id] = {
+          id: topping.id,
+          name: topping.name,
+          count: 1,
+        };
+      }
+      return acc;
+    }, {});
+  // Sort them based on their count
+  const sortedToppings = Object.values(counts).sort(
+    (a, b) => b.count - a.count
+  );
+  return sortedToppings;
+}
+
+export default function ToppingsFilter() {
+  // Get a list of all the pizzas with their toppings
+  const { pizzas } = useStaticQuery(graphql`
+    query {
+      pizzas: allSanityPizza {
+        nodes {
+          toppings {
+            id
+            name
+          }
+        }
+      }
+    }
+  `);
+  // Count how many pizzas are in each topping
+  const toppingsWithCounts = countPizzasInToppings(pizzas.nodes);
+  // Loop over the list of toppings and display the topping and the count of pizzas in that topping
+  // Link it up ...
+  return (
+    <ToppingStyles>
+      {toppingsWithCounts.map((topping) => (
+        <Link key={topping.id} to={`/topping/${topping.name}`}>
+          <span className="name">{topping.name}</span>
+          <span className="count">{topping.count}</span>
+        </Link>
+      ))}
+    </ToppingStyles>
+  );
+}
