53e099d5f756dc2490da5b36e6957e84df567fe6
Chris <spidergon@gmail.com>
2020-09-29 07:50:20 -0300

    Dynamically creating toppings pages

--- gatsby/gatsby-node.js
+++ gatsby/gatsby-node.js
@@ -1,7 +1,7 @@
 import path from 'path';
 
 async function turnPizzasIntoPages({ graphql, actions }) {
-  // 1. Get a template for this pagee
+  // 1. Get a template for this page
   const pizzaTemplate = path.resolve('./src/templates/Pizza.js');
   // 2. Query all pizzas
   const { data } = await graphql(`
@@ -28,10 +28,43 @@ async function turnPizzasIntoPages({ graphql, actions }) {
   });
 }
 
+async function turnToppingsIntoPages({ graphql, actions }) {
+  console.log(`Turning the toppings into Pages!!!`);
+  // 1. Get the template
+  const toppingTemplate = path.resolve('./src/pages/pizzas.js');
+  // 2. query all the toppings
+  const { data } = await graphql(`
+    query {
+      toppings: allSanityTopping {
+        nodes {
+          name
+          id
+        }
+      }
+    }
+  `);
+  // 3. createPage for that topping
+  // 4. Pass topping data to pizza.js
+  data.toppings.nodes.forEach((topping) => {
+    actions.createPage({
+      path: `topping/${topping.name}`,
+      component: toppingTemplate,
+      context: {
+        topping: topping.name,
+        toppingRegex: `/${topping.name}/i`,
+      },
+    });
+  });
+}
+
 export async function createPages(params) {
   // Create pages dynamically
+  // Wait for all promises to be resolved before finishing this function
   // 1. Pizzas
-  await turnPizzasIntoPages(params);
   // 2. Toppings
+  await Promise.all([
+    turnPizzasIntoPages(params),
+    turnToppingsIntoPages(params),
+  ]);
   // 3. Slicemasters
 }
--- gatsby/src/components/ToppingsFilter.js
+++ gatsby/src/components/ToppingsFilter.js
@@ -19,7 +19,8 @@ const ToppingStyles = styled.div`
       background: white;
       padding: 2px 5px;
     }
-    .active {
+    &[aria-current='page'],
+    &.active {
       background: var(--yellow);
     }
   }
@@ -53,7 +54,7 @@ function countPizzasInToppings(pizzas) {
   return sortedToppings;
 }
 
-export default function ToppingsFilter() {
+export default function ToppingsFilter({ activeTopping }) {
   // Get a list of all the pizzas with their toppings
   const { pizzas } = useStaticQuery(graphql`
     query {
@@ -73,8 +74,16 @@ export default function ToppingsFilter() {
   // Link it up ...
   return (
     <ToppingStyles>
+      <Link to="/pizzas">
+        <span className="name">All</span>
+        <span className="count">{pizzas.nodes.length}</span>
+      </Link>
       {toppingsWithCounts.map((topping) => (
-        <Link key={topping.id} to={`/topping/${topping.name}`}>
+        <Link
+          key={topping.id}
+          to={`/topping/${topping.name}`}
+          className={topping.name === activeTopping ? 'active' : ''}
+        >
           <span className="name">{topping.name}</span>
           <span className="count">{topping.count}</span>
         </Link>
