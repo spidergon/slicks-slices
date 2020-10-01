b6131609b8b7b9ac993dd49e7881d3b98b8f274d
Chris <spidergon@gmail.com>
2020-10-01 16:47:56 -0300

    Order page - form custom hooks and styling

--- gatsby/src/pages/index.js
+++ gatsby/src/pages/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import SEO from '../components/SEO';
 
 export default function HomePage() {
   return (
     <>
+      <SEO title="Hot Now" />
       <p>Hey! I'm the home page</p>
     </>
   );
