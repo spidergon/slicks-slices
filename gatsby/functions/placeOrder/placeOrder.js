27902f609cc0717919557eed99160b1f4bdb65e5
Chris <spidergon@gmail.com>
2020-10-08 13:03:55 -0300

    placeOrder serverless function

--- gatsby/functions/placeOrder/placeOrder.js
+++ gatsby/functions/placeOrder/placeOrder.js
@@ -1,5 +1,28 @@
 const nodemailer = require('nodemailer');
 
+function generateOrderEmail({ order, total }) {
+  return `<div>
+    <h2>Your Recent Order for ${total}</h2>
+    <p>Please start walking over, we will have your order ready in the next 20 mins.</p>
+    <ul>
+      ${order
+        .map(
+          (item) => `<li>
+        <img src="${item.thumbnail}" alt="${item.name}"/>
+        ${item.size} ${item.name} - ${item.price}
+      </li>`
+        )
+        .join('')}
+    </ul>
+    <p>Your total is <strong>${total}</strong> due at pickup</p>
+    <style>
+      ul {
+        list-style: none;
+      }
+    </style>
+  </div>`;
+}
+
 // Create a transport for nodemailer
 const transporter = nodemailer.createTransport({
   host: process.env.MAIL_HOST,
@@ -11,15 +34,34 @@ const transporter = nodemailer.createTransport({
 });
 
 exports.handler = async (event, context) => {
-  // Test send an email
+  const body = JSON.parse(event.body);
+  // Validate the data coming in is correct
+  const requiredFields = ['email', 'name', 'order'];
+
+  for (const field of requiredFields) {
+    console.log(`Checking that ${field} is good`);
+    if (!body[field]) {
+      return {
+        statusCode: 400,
+        body: JSON.stringify({
+          message: `Oops! You are missing the ${field} field`,
+        }),
+      };
+    }
+  }
+
+  // Send the email
   const info = await transporter.sendMail({
     from: 'Slicks Slices <slick@example.com>',
-    to: 'orders@example.com',
+    to: `${body.name} <${body.email}>, orders@example.com`,
     subject: 'New order!',
-    html: `<p>Your new pizza order is here!</p>`,
+    html: generateOrderEmail({ order: body.order, total: body.total }),
   });
+
   return {
     statusCode: 200,
-    body: JSON.stringify(info),
+    body: JSON.stringify({ message: 'Success' }),
   };
+
+  // Send the success or error message
 };
